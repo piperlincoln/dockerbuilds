8872f8ca4e1681d522c97ddf9fa7db9a3adff576
piperlincoln <plincoln@wisc.edu>
2019-01-10 12:40:38 -0600

    testing

--- SessionReplaceData.py
+++ SessionReplaceData.py
@@ -9,7 +9,7 @@ from pymoab import core, tag, types
 def parse_arguments():
     """
     Parse the argument list and return the location of a session file, a new
-    geometry file, and a new data file.
+    geometry file, a new data file, and the name of the new session file.
 
     Input:
     ______
@@ -18,7 +18,8 @@ def parse_arguments():
     Returns:
     ________
        args: Namespace
-           User supplied session file, geometry file, and data file location.
+           User supplied session file, geometry file, and data file location,
+           and name of new session file.
     """
 
     parser = argparse.ArgumentParser(description="Replace data in session file.")
@@ -35,13 +36,17 @@ def parse_arguments():
                         type=str,
                         help="Provide a path to the data file."
                         )
+    parser.add_argument("-o", "--outputfile",
+                        type=str,
+                        help="Provide a name for the new session file."
+                        )
 
     args = parser.parse_args()
 
     return args
 
 
-def replace_session_data(session_file, geometry_file = None, data_file = None):
+def replace_session_data(session_file, geometry_file = None, data_file = None, output_file = None):
     """
     Convert geometry file to stl, convert data file to vtk, replace the data
     in the session file, and open VisIt with the updated session file.
@@ -54,6 +59,8 @@ def replace_session_data(session_file, geometry_file = None, data_file = None):
            User supplied file containing geometry of interest.
        data_file: h5m or vtk file
            User supplied file containing data of interest.
+       output_file: str
+           User supplied name of the new session file.
 
     Returns:
     ________
@@ -78,13 +85,19 @@ def replace_session_data(session_file, geometry_file = None, data_file = None):
         # Convert the data file to the proper format.
         data_file = py_mb_convert(data_file, ".vtk")
 
-    # Replace the database(s) in VisIt and recreate the sessionfile.
-    Vi.AddArgument("-movie")
-    Vi.AddArgument("-sessionfile {}".format(session_file))
+    # Restore the session file in VisIt and replace the geometry and/or data file.
     Vi.LaunchNowin()
-    Vi.OverlayDatabase(data_file)
+    Vi.RestoreSession(session_file, 0)
+    Vi.ReplaceDatabase(PLACEHOLDER)
 
-    # Open VisIt with the new data loaded in the old plots. (Lines 235-242)
+    # Create the new session file.
+    if output_file is None:
+        Vi.SaveSession(session_file)
+    else:
+        Vi.SaveSession(output_file)
+    Vi.Close()
+
+    # Open the VisIt GUI with the new session file.
 
 
 def main():
@@ -93,7 +106,7 @@ def main():
   args = parse_arguments()
 
   # Replace the data in the session file with what the user supplied.
-  replace_session_data(args.sessionfile, args.geofile, args.datafile)
+  replace_session_data(args.sessionfile, args.geofile, args.datafile, args.outputfile)
 
 
 if __name__ == "__main__":
